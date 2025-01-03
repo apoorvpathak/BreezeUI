da9579503992f8c8f1851b09301748be7d70f6d0
Apoorv Pathak <apoorvworks@gmail.com>
2025-01-03 17:38:08 +0530

    add Navbar to error404 page

--- src/Pages/Error404.jsx
+++ src/Pages/Error404.jsx
@@ -1,13 +1,17 @@
 import { Link } from "react-router-dom";
+import Navbar from "../Components/Navbar";
 function Error404() {
     return (
         <>
-            <div className="flex justify-center items-center h-screen">
-                <div className="text-white font-inter space-y-5 justify-items-center">
-                    <p className="text-2xl mb-6">Looks Like you are lost!</p>
-                    <Link to={'/'} className="outline outline-white mt-4 p-1 outline-1 rounded-sm">Go To Home</Link>
-                </div>
-            </div>
+<div className="flex flex-col min-h-screen">
+    <Navbar/>
+    <div className="flex-1 flex justify-center items-center">
+        <div className="text-white font-inter space-y-5 justify-items-center">
+            <p className="text-2xl mb-6">Looks Like you are lost!</p>
+            <Link to={'/'} className="outline outline-white mt-4 p-1 outline-1 rounded-sm">Go To Home</Link>
+        </div>
+    </div>
+</div>
         </>
     );
 }
