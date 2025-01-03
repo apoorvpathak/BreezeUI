1c80dfca6c2e7a6cf4fdd1c8451950569c12a846
Apoorv Pathak <apoorvworks@gmail.com>
2025-01-03 18:13:05 +0530

    fix sizing

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -5,7 +5,7 @@ function Navbar() {
         <>
             <div className="text-white font-inter m-5">
                 <div className="flex space-x-2 justify-around items-center">
-                    <Link to={'/'} className="font-semibold text-2xl">Breeze UI</Link>
+                    <Link to={'/'} className="font-semibold text-xl">Breeze UI</Link>
                     <div  className="flex space-x-4 items-center md:space-x-12">
                         <Link to={'/Docs'} className="font-medium text-lg text-gray-300 hover:text-white hover:cursor-pointer">Docs</Link>
                         <Link to={'/Components'} className="font-medium text-lg text-gray-300 hover:text-white hover:cursor-pointer">Components</Link>
