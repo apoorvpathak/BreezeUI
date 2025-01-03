8cee1dc0a321f1c26be76e0affca21ae0056fcc9
Apoorv Pathak <apoorvworks@gmail.com>
2025-01-03 17:27:44 +0530

    implement Link from react-router-dom

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -1,12 +1,14 @@
+import { Link } from "react-router-dom";
+
 function Navbar() {
     return (
         <>
             <div className="text-white font-inter m-5">
                 <div className="flex space-x-2 justify-around items-center">
-                    <p className="font-semibold text-2xl">Breeze UI</p>
+                    <Link to={'/'} className="font-semibold text-2xl">Breeze UI</Link>
                     <div  className="flex space-x-4 items-center md:space-x-12">
-                        <p className="font-medium text-lg text-gray-300 hover:text-white hover:cursor-pointer">Docs</p>
-                        <p className="font-medium text-lg text-gray-300 hover:text-white hover:cursor-pointer">Components</p>
+                        <Link to={'/Docs'} className="font-medium text-lg text-gray-300 hover:text-white hover:cursor-pointer">Docs</Link>
+                        <Link to={'/Components'} className="font-medium text-lg text-gray-300 hover:text-white hover:cursor-pointer">Components</Link>
                     </div>
                 </div>
             </div>
