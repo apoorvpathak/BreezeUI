c6f88f104567292e1581250a628e1825334cbf4c
Apoorv Pathak <apoorvworks@gmail.com>
2025-01-12 22:12:51 +0530

    update cta & create onClick function

--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -1,12 +1,17 @@
 import {Link} from 'react-router-dom';
+import StarSvg from '../assets/StarIcon.svg'
 
 function Home() {
+    function handleStarGitHub(){
+        const githubUrl = 'https://github.com/apoorvpathak/BreezeUI'
+        window.open(githubUrl, '_blank')
+    }
     return ( 
 <>
     <div className="max-w-4xl mx-auto my-16 text-center px-4 py-16 font-inter">
         <button className='font-inter text-white mt-4 mb-4 bg-gray-700 px-2 rounded-full'>
-            <div className='flex items-center space-x-2 '>
-            <img src="https://www.svgrepo.com/show/448234/linkedin.svg" className='h-7' alt="" /> <p>Follow on LinkedIn ↗</p>
+            <div onClick={handleStarGitHub} className='flex items-center space-x-2 '>
+            <img src={StarSvg} className='h-4' alt="" /> <p>Star on GitHub ↗</p>
             </div>
         </button>
         <h1 className="text-4xl md:text-5xl font-bold text-white mb-6 font-inter tracking-tight">
