b27f2148d9151397180cc643220dc7891c5a5a9d
Apoorv Pathak <apoorvworks@gmail.com>
2025-01-12 22:04:28 +0530

    update file location

---
+++ src/Pages/ComponentsPage/ComponentsPage.jsx
@@ -0,0 +1,11 @@
+import Navbar from '../../Components/Navbar';
+function ComponentsPage() {
+    return ( 
+        <>
+        <Navbar/>
+        <h1 className='text-white font-inter '>Currently there are only a few options for Components</h1>
+        </>
+     );
+}
+
+export default ComponentsPage;
