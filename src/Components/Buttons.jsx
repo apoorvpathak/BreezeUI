493aee6b9b8ae15bfaa2623721db323ac0235949
Apoorv Pathak <apoorvworks@gmail.com>
2025-01-13 22:44:38 +0530

    create basic buttons

---
+++ src/Components/Buttons.jsx
@@ -0,0 +1,46 @@
+function Buttons() {
+    return (
+        <>
+            <div className='text-white font-inter mt-12 md:flex md:items-center md:justify-evenly'>
+
+                <div className="flex justify-center md:block">
+                    
+                <p className='text-2xl font-medium text-gray-200 px-4'>Buttons</p>
+                </div>
+
+
+
+                <div className='space-y-4'>
+                    <div className='md:flex space gap-x-4'>
+                        <div className='w-64 h-32 bg-gray-200 flex items-center justify-center'>
+                            <div>
+                                <button className='bg-blue-500 px-6 font-medium py-2 font-inter rounded-md hover:bg-blue-600 hover:font-normal'>Button 1</button>
+                            </div>
+                        </div>
+                        <div className='w-64 h-32 bg-gray-200 flex items-center justify-center'>
+                            <div>
+                                <button className='bg-purple-500 px-6 font-medium py-2 font-inter rounded-md hover:bg-purple-600 hover:font-normal'>Button 2</button>
+                            </div>
+                        </div>
+                    </div>
+                    <div className='md:flex space gap-x-4'>
+                        <div className='w-64 h-32 bg-gray-200 flex items-center justify-center'>
+                            <div>
+                                <button className='text-black font-medium ring-1 ring-black px-6 py-2 rounded-md hover:font-normal hover:bg-black hover:text-white'>Button 3</button>
+                            </div>
+                        </div>
+                        <div className='w-64 h-32 bg-gray-950 flex items-center justify-center'>
+                            <div>
+                                <button className='text-white font-medium ring-1 ring-white px-6 py-2 rounded-md hover:font-normal hover:bg-white hover:text-black'>Button 4</button>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
+
+            </div>
+        </>
+    );
+}
+
+export default Buttons;
