faf36fe4da7d5950a00550498e24b432a3f16fde
Apoorv Pathak <apoorvworks@gmail.com>
2025-01-13 22:44:53 +0530

    add buttons

--- src/Pages/ComponentsPage/ComponentsPage.jsx
+++ src/Pages/ComponentsPage/ComponentsPage.jsx
@@ -1,11 +1,15 @@
+import Buttons from '../../Components/Buttons';
 import Navbar from '../../Components/Navbar';
 function ComponentsPage() {
-    return ( 
+    return (
         <>
-        <Navbar/>
-        <h1 className='text-white font-inter '>Currently there are only a few options for Components</h1>
+            <Navbar />
+            <div className='flex justify-center mt-24'>
+                <p className='text-white font-inter font-bold  text-center max-w-md text-2xl md:max-w-lg md:text-4xl'> Choose from the following wide varieties of Components.  </p>
+            </div>
+            <Buttons/>
         </>
-     );
+    );
 }
 
 export default ComponentsPage;
