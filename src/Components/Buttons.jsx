e39ef3d383cf7e598022d342ca09d02962574e5b
Apoorv Pathak <apoorvworks@gmail.com>
2025-01-15 18:13:10 +0530

    add toast message

--- src/Components/Buttons.jsx
+++ src/Components/Buttons.jsx
@@ -1,22 +1,24 @@
+import { Toaster, toast } from 'sonner';
+
 function Buttons() {
     function handleClickBtn1(){
         navigator.clipboard.writeText("<button className='bg-blue-500 px-6 font-medium text-white py-2 rounded-md hover:bg-blue-600 hover:font-normal'>Button 1</button>").then(()=>{
-            // display toast message
+            toast.success('Component Copied Successfully')
         })
     }
     function handleClickBtn2(){
         navigator.clipboard.writeText("<button className='bg-purple-500 text-white px-6 font-medium py-2 rounded-md hover:bg-purple-600 hover:font-normal'>Button 2</button>").then(()=>{
-            // display toast message
+            toast.success('Component Copied Successfully')
         })
     }
     function handleClickBtn3(){
         navigator.clipboard.writeText("<button className='text-black font-medium ring-1 ring-black px-6 py-2 rounded-md hover:font-normal hover:bg-black hover:text-white'>Button 3</button>").then(()=>{
-            // display toast message
+            toast.success('Component Copied Successfully')
         })
     }
     function handleClickBtn4(){
         navigator.clipboard.writeText("<button className='text-white font-medium ring-1 ring-white px-6 py-2 rounded-md hover:font-normal hover:bg-white hover:text-black'>Button 4</button>").then(()=>{
-            // display toast message
+            toast.success('Component Copied Successfully')
         })
     }
     return (
@@ -59,6 +61,7 @@ function Buttons() {
 
 
             </div>
+            <Toaster />
         </>
     );
 }
