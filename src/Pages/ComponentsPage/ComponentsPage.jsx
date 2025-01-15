ee422671526865d16beef6586f741be2b2f7b480
Apoorv Pathak <apoorvworks@gmail.com>
2025-01-15 15:16:39 +0530

    add instruction for copying code

--- src/Pages/ComponentsPage/ComponentsPage.jsx
+++ src/Pages/ComponentsPage/ComponentsPage.jsx
@@ -4,8 +4,9 @@ function ComponentsPage() {
     return (
         <>
             <Navbar />
-            <div className='flex justify-center mt-24'>
+            <div className='flex-col items-center flex space-y-4 mt-24'>
                 <p className='text-white font-inter font-bold  text-center max-w-md text-2xl md:max-w-lg md:text-4xl'> Choose from the following wide varieties of Components.  </p>
+                <p className='text-white font-inter'>Click on the component to copy code.</p>
             </div>
             <Buttons/>
         </>
