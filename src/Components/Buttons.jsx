2472cfa272248420f26dfee510dc0a9961301066
Apoorv Pathak <apoorvworks@gmail.com>
2025-01-20 21:09:47 +0530

    Refactor Copy  function (#1)

--- src/Components/Buttons.jsx
+++ src/Components/Buttons.jsx
@@ -1,24 +1,9 @@
 import { Toaster, toast } from 'sonner';
 
 function Buttons() {
-    function handleClickBtn1(){
-        navigator.clipboard.writeText("<button className='bg-blue-500 px-6 font-medium text-white py-2 rounded-md hover:bg-blue-600 hover:font-normal'>Button 1</button>").then(()=>{
-            toast.success('Component Copied Successfully')
-        })
-    }
-    function handleClickBtn2(){
-        navigator.clipboard.writeText("<button className='bg-purple-500 text-white px-6 font-medium py-2 rounded-md hover:bg-purple-600 hover:font-normal'>Button 2</button>").then(()=>{
-            toast.success('Component Copied Successfully')
-        })
-    }
-    function handleClickBtn3(){
-        navigator.clipboard.writeText("<button className='text-black font-medium ring-1 ring-black px-6 py-2 rounded-md hover:font-normal hover:bg-black hover:text-white'>Button 3</button>").then(()=>{
-            toast.success('Component Copied Successfully')
-        })
-    }
-    function handleClickBtn4(){
-        navigator.clipboard.writeText("<button className='text-white font-medium ring-1 ring-white px-6 py-2 rounded-md hover:font-normal hover:bg-white hover:text-black'>Button 4</button>").then(()=>{
-            toast.success('Component Copied Successfully')
+    function handleButtonCopy(e){
+        navigator.clipboard.writeText(e.target.outerHTML).then(()=>{
+            toast.success(`${e.target.innerText} Copied Successfully`)
         })
     }
     return (
@@ -34,26 +19,26 @@ function Buttons() {
 
                 <div className='space-y-4 flex flex-col items-center'>
                     <div className='md:flex space-y-4 md:space-y-0 gap-x-4'>
-                        <div onClick={handleClickBtn1} className='w-64 h-32 bg-gray-200 flex items-center justify-center'>
+                        <div  className='w-64 h-32 bg-gray-200 flex items-center justify-center'>
                             <div>
-                                <button className='bg-blue-500 px-6 font-medium text-white py-2 rounded-md hover:bg-blue-600 hover:font-normal'>Button 1</button>
+                                <button onClick={handleButtonCopy} className='bg-blue-500 px-6 font-medium text-white py-2 rounded-md hover:bg-blue-600 hover:font-normal'>Button 1</button>
                             </div>
                         </div>
-                        <div onClick={handleClickBtn2} className='w-64 h-32 bg-gray-200 flex items-center justify-center'>
+                        <div  className='w-64 h-32 bg-gray-200 flex items-center justify-center'>
                             <div>
-                                <button className='bg-purple-500 text-white px-6 font-medium py-2 rounded-md hover:bg-purple-600 hover:font-normal'>Button 2</button>
+                                <button onClick={handleButtonCopy} className='bg-purple-500 text-white px-6 font-medium py-2 rounded-md hover:bg-purple-600 hover:font-normal'>Button 2</button>
                             </div>
                         </div>
                     </div>
                     <div className='md:flex space-y-4 md:space-y-0 gap-x-4'>
-                        <div onClick={handleClickBtn3} className='w-64 h-32 bg-gray-200 flex items-center justify-center'>
+                        <div  className='w-64 h-32 bg-gray-200 flex items-center justify-center'>
                             <div>
-                                <button className='text-black font-medium ring-1 ring-black px-6 py-2 rounded-md hover:font-normal hover:bg-black hover:text-white'>Button 3</button>
+                                <button onClick={handleButtonCopy} className='text-black font-medium ring-1 ring-black px-6 py-2 rounded-md hover:font-normal hover:bg-black hover:text-white'>Button 3</button>
                             </div>
                         </div>
-                        <div onClick={handleClickBtn4} className='w-64 h-32 bg-gray-950 flex items-center justify-center'>
+                        <div  className='w-64 h-32 bg-gray-950 flex items-center justify-center'>
                             <div>
-                                <button className='text-white font-medium ring-1 ring-white px-6 py-2 rounded-md hover:font-normal hover:bg-white hover:text-black'>Button 4</button>
+                                <button onClick={handleButtonCopy} className='text-white font-medium ring-1 ring-white px-6 py-2 rounded-md hover:font-normal hover:bg-white hover:text-black'>Button 4</button>
                             </div>
                         </div>
                     </div>
